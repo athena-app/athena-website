e54aefd2806e85421a36380ba1a3f01a8c9d59c1
Bryson Reece <breece@wellcaddie.com>
2018-03-03 19:29:17 +0000

    Fixed active navs, changed icons

--- resources/views/constant-nav-items.blade.php
+++ resources/views/constant-nav-items.blade.php
@@ -1,27 +1,27 @@
 <li class="nav-item nav-link">
-    <a href="/favorites">
+    <a class="nav-link" href="/favorites">
         <i class="fa fa-nav fa-fw fa-btn fa-star"></i>
         My Favorites
     </a>
 </li>
 
 <li class="nav-item nav-link">
-    <a href="/subjects">
-        <i class="fa fa-nav fa-fw fa-btn fa-arrow-left"></i>
+    <a class="nav-link" href="/subjects">
+        <i class="fa fa-nav fa-fw fa-btn fa-book"></i>
         Subjects
     </a>
 </li>
 
 <li class="nav-item nav-link">
-    <a href="/notes">
-        <i class="fa fa-nav fa-fw fa-btn fa-arrow-left"></i>
+    <a class="nav-link" href="/notes">
+        <i class="fa fa-nav fa-fw fa-btn fa-sticky-note"></i>
         Notes
     </a>
 </li>
 
 <li class="nav-item nav-link">
-    <a href="/chat">
-        <i class="fa fa-nav fa-fw fa-btn fa-arrow-left"></i>
+    <a class="nav-link" href="/chat">
+        <i class="fa fa-nav fa-fw fa-btn fa-comment"></i>
         Chat
     </a>
 </li>
