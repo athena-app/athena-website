320220625034322b8bb08e332eba1d23eea25751
Bryson Reece <breece@wellcaddie.com>
2018-03-04 11:30:05 +0000

    Chat implemented

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <home :user="user" inline-template>
 <div class="card card-default">
-    <div class="card-header">{{__('Dashboard')}}</div>
+    <div class="card-header">Chat</div>
 
     <div class="card-body">
         <div id="tlkio" class="embed-responsive embed-responsive-16by9" data-channel="{{ 'athena-team-' . Auth::user()->currentTeam->id }}" data-theme="theme--minimal"></div><script async src="http://tlk.io/embed.js" type="text/javascript"></script>
