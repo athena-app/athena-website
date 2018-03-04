6b8faa2ddb6df6b1f71570848417088cd959c7c5
Bryson Reece <breece@wellcaddie.com>
2018-03-04 08:23:30 +0000

    Added 3D model cards

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -31,7 +31,25 @@ class LessonController extends Controller
      */
     public function show(Lesson $lesson)
     {
+        $models = array();
+        $token = "9110d0c7b73a4ecf344672c185181b36";
+        $keywords = explode(',', $lesson->keywords);
+
+        foreach ($keywords as $keyword) {
+            $keyword = trim($keyword);
+            $url = "https://api.thingiverse.com/tags/$keyword/things?access_token=$token";
+            $request = @file_get_contents($url);
+
+            if ($request) {
+                $response = json_decode($request);
+
+                // Models found
+                $models = $response;
+            }
+        }
+
         return view('lessons.show')
-            ->with('lesson', $lesson);
+            ->with('lesson', $lesson)
+            ->with('models', $models);
     }
 }
--- resources/views/lessons/show.blade.php
+++ resources/views/lessons/show.blade.php
@@ -21,17 +21,33 @@
                 <h4 class="my-3 lead d-block">Description</h4>
             </div>
             <div class="row mx-2">
-                <p>{{ $lesson->description }}</p>
-            </div>
-        </div>
-        <div class="container">
-            <div class="row mx-2">
-                <h4 class="my-3 lead d-block">3D Models related to "{{ $lesson->keywords }}"</h4>
-            </div>
-            <div class="row mx-2">
-                <p>//TODO</p>
+                <p>{{ empty($lesson->description) ? 'No description found.' : $lesson->description }}</p>
             </div>
         </div>
     </div>
 </div>
+
+@if (!empty($models))
+    <div class="card card-default">
+        <div class="card-header clearfix">
+            <div class="pull-left">
+                3D Models related to "{{ $lesson->keywords }}" from Thingiverse
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        @foreach ($models as $model)
+            <div class="col-md-3">
+                <div class="card">
+                    <img class="card-img-top" src="{{ str_replace('medium', 'large', $model->thumbnail) }}" alt="Card image cap">
+                    <div class="card-block p-3">
+                        <h4 class="card-title">{{ $model->name }}</h4>
+                        <p class="card-text"><small class="text-muted">Uploaded by {{ $model->creator->name }}</small></p>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+@endif
 @stop
