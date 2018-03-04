372779a74a7bb728dca8a597f0068ab2105c9ac6
Bryson Reece <breece@wellcaddie.com>
2018-03-04 11:57:29 +0000

    Random fixes

--- resources/views/constant-nav-items.blade.php
+++ resources/views/constant-nav-items.blade.php
@@ -1,26 +1,26 @@
 <li class="nav-item">
-    <a class="nav-link" href="/favorites">
+    <a class="nav-link {{ collect(Request::segments())->first() == 'favorites' ? 'active' : '' }}" href="/favorites">
         <i class="fa fa-nav fa-fw fa-btn fa-star"></i>
         My Favorites
     </a>
 </li>
 
 <li class="nav-item">
-    <a class="nav-link" href="/subjects">
+    <a class="nav-link {{ collect(Request::segments())->first() == 'subjects' ? 'active' : '' }}" href="/subjects">
         <i class="fa fa-nav fa-fw fa-btn fa-book"></i>
         Subjects
     </a>
 </li>
 
 <li class="nav-item">
-    <a class="nav-link" href="/notes">
+    <a class="nav-link {{ collect(Request::segments())->first() == 'notes' ? 'active' : '' }}" href="/notes">
         <i class="fa fa-nav fa-fw fa-btn fa-sticky-note"></i>
         Notes
     </a>
 </li>
 
 <li class="nav-item">
-    <a class="nav-link" href="/chat">
+    <a class="nav-link {{ collect(Request::segments())->first() == 'chat' ? 'active' : '' }}" href="/chat">
         <i class="fa fa-nav fa-fw fa-btn fa-comment"></i>
         Chat
     </a>
