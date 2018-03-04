cb0710187ae05df6ae5e22fa599792eddfbb01ab
Ryan Dobyns <ryandobyns@gmail.com>
2018-03-04 11:21:20 +0000

    Fixed note nav-bar

--- resources/views/notes/nav-items.blade.php
+++ resources/views/notes/nav-items.blade.php
@@ -4,9 +4,3 @@
         {{__('Add Note')}}
     </a>
 </li>
-<li class="nav-item ">
-    <a class="nav-link" href="#" data-toggle="modal" data-target="#edit-note-modal">
-        <i class="fa fa-nav fa-fw fa-btn fa-pencil"></i>
-        {{__('Edit Note')}}
-    </a>
-</li>
