09297a74d384d2a5b8e18bbb09a32e353bfae341
Ryan Dobyns <ryandobyns@gmail.com>
2018-03-04 07:59:33 +0000

    Branding

--- resources/views/vendor/spark/nav/user.blade.php
+++ resources/views/vendor/spark/nav/user.blade.php
@@ -10,7 +10,9 @@
     <nav class="navbar navbar-light navbar-expand-md navbar-spark">
         <div class="container" v-if="user">
             <!-- Branding Image -->
-            @include('spark::nav.brand')
+            <a href="/home">
+                <img src="img/athena-logo.png" width="75px" height="75px" alt="Athena">
+            </a>
 
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
